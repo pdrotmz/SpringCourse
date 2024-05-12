1de6ba9db03ebc6affb5293936fdffd8bf6cc233
pdrotmz <pedrotomaz0210@gmail.com>
2024-05-12 20:20:01 -0300

    feat(StudentService): student information setup

---
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,20 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+public class StudentService {
+    	public List<Student> getStudents(){
+		return List.of(
+			// Creating a new instance of Student
+			new Student(
+				1L,
+				"Pedro",
+				"pedrotomaz@gmail.com",
+				LocalDate.of(2003, Month.FEBRUARY, 03),
+				21
+			)
+		);
+	}
+}
