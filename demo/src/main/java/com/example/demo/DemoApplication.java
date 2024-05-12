a3d396291b0e74e63f1a619336deffb0a5797bc0
pdrotmz <pedrotomaz0210@gmail.com>
2024-05-12 19:55:58 -0300

    feat(DemoApplication): new instance created

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,15 @@
 package com.example.demo;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.student.Student;
 
 @SpringBootApplication
 @RestController // Spring recognize that DemoApplication is a RestController
@@ -18,8 +22,17 @@ public class DemoApplication {
 	}
 
 	@GetMapping("") // url for the function in browser, empty means default as an index
-	public List<String> hello(){
-		return List.of("Hello", "World");
+	public List<Student> hello(){
+		return List.of(
+			// Creating a new instance of Student
+			new Student(
+				1L,
+				"Pedro",
+				"pedrotomaz@gmail.com",
+				LocalDate.of(2003, Month.FEBRUARY, 03),
+				21
+			)
+		);
 	}
 
 }
