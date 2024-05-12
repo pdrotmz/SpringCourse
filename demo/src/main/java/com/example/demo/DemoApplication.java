92e62e779421f45decae82ef909e7bf94337c05a
pdrotmz <pedrotomaz0210@gmail.com>
2024-05-12 20:14:10 -0300

    feat(DemoApplication): remove all of it

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,9 @@
 package com.example.demo;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.student.Student;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController // Spring recognize that DemoApplication is a RestController
@@ -20,19 +14,4 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
-	@GetMapping("") // url for the function in browser, empty means default as an index
-	public List<Student> hello(){
-		return List.of(
-			// Creating a new instance of Student
-			new Student(
-				1L,
-				"Pedro",
-				"pedrotomaz@gmail.com",
-				LocalDate.of(2003, Month.FEBRUARY, 03),
-				21
-			)
-		);
-	}
-
 }
