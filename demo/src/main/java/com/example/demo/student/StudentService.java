eb5e9b2872c59672531169a6672f7affcc5f2f9c
pdrotmz <pedrotomaz0210@gmail.com>
2024-05-19 23:23:41 -0300

    feat(StudentService): JPA integration done

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -1,23 +1,23 @@
 package com.example.demo.student;
 
-import java.time.LocalDate;
-import java.time.Month;
+
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
-    	public List<Student> getStudents(){
-		return List.of(
-			// Creating a new instance of Student
-			new Student(
-				1L,
-				"Pedro",
-				"pedrotomaz@gmail.com",
-				LocalDate.of(2003, Month.FEBRUARY, 03),
-				21
-			)
-		);
+
+	private final StudentRepository studentRepository;
+
+	@Autowired
+    public StudentService(StudentRepository studentRepository) {
+		this.studentRepository = studentRepository;
+	}
+
+	public List<Student> getStudents(){
+		// Return what he found
+		return studentRepository.findAll();
 	}
 }
