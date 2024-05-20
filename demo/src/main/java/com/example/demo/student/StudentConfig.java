5d87b3d57bc339afab99da256b239d7d013ca765
pdrotmz <pedrotomaz0210@gmail.com>
2024-05-19 23:24:15 -0300

    feat(StudentConfig): StudentConfig created

---
+++ demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,37 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            	Student Pedro = new Student(
+				1L, "Pedro",
+				"pedrotomaz@gmail.com",
+				LocalDate.of(2003, Month.FEBRUARY, 03),
+				21
+			);
+
+            Student Gabi = new Student(
+				2L, "Gabi",
+				"gabizinha@gmail.com",
+				LocalDate.of(2003, Month.MARCH, 07),
+				21
+			);
+
+            // Save Student
+            repository.saveAll(
+                List.of(Pedro, Gabi)
+            );
+        };
+    }
+}
