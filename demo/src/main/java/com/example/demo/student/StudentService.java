082b466a818c6c0b3515c2f97b9fd1e3de361434
pdrotmz <pedrotomaz0210@gmail.com>
2024-06-02 21:54:01 -0300

    feat(StudentService.java): created method to print the date of students

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -20,4 +20,8 @@ public class StudentService {
 		// Return what he found
 		return studentRepository.findAll();
 	}
+
+    public void addNewStudent(Student student) {
+		System.out.println(student);
+    }
 }
